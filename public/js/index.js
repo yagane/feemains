5add053f5f445bf8824819ee2278f5961834ce75
YanisMillasseau <millassy@3il.fr>
2026-02-03 16:57:24 +0100

    Update js src php

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 async function updateAuthUI() {
-    const response = await fetch('../backend/php/check_auth.php');
+    const response = await fetch('../../backend/php/check_auth.php');
     const data = await response.json();
     const authLink = document.getElementById('auth-link');
     const userGreeting = document.getElementById('user-greeting');
