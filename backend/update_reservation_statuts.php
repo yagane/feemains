e5ac2355c24ecae1f08a482e52a64e770c308f94
YanisMillasseau <millassy@3il.fr>
2026-02-16 17:40:32 +0100

    Link prestation to Router

--- backend/update_reservation_statuts.php
+++ backend/update_reservation_statuts.php
@@ -1,21 +1,21 @@
 <?php
 // Connexion à la base de données
 $db = new PDO(
-    "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']}",
-    $env['DB_USER'],
-    $env['DB_PASS'],
+    "mysql:host=db5019347394.hosting-data.io;dbname=dbs15148152",
+    'dbu1416311',
+    'Sx6ZD7KhW4n/sc+',
     [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
 );
 
 // Mettre à jour les réservations dont la date est passée
-$stmt = $db->prepare("
-    UPDATE reservations
+$stmt = $db->prepare(
+    "UPDATE reservations
     SET statut = 'passé'
     WHERE (statut = 'en attente')
-    AND (date_reservation < NOW())
-");
+    AND (date_reservation < NOW())"
+);
 
 $stmt->execute();
 ?>
