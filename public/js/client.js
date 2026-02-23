76b1baa4b8431b9f738aa353b5d4896cbd190954
YanisMillasseau <millassy@3il.fr>
2026-02-23 15:17:27 +0100

    Update historique resumé

--- public/js/client.js
+++ public/js/client.js
@@ -104,15 +104,15 @@ async function loadHistoric() {
                 modal.className = 'modal-backdrop';
 
                 modal.innerHTML = `
+                    <div class="modal-nav">
+                        <button class="close-button">
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                                <line x1="18" y1="6" x2="6" y2="18"></line>
+                                <line x1="6" y1="6" x2="18" y2="18"></line>
+                            </svg>
+                        </button>
+                    </div>
                     <div class="modal-content">
-                        <div class="modal-nav">
-                            <button class="close-button">
-                                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                    <line x1="18" y1="6" x2="6" y2="18"></line>
-                                    <line x1="6" y1="6" x2="18" y2="18"></line>
-                                </svg>
-                            </button>
-                        </div>
                         <div class="modal-header">
                             <h3>Reservation du ${formattedDate.split(' ')[0]}</h3>
                         </div>
