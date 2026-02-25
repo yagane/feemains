b65b343eabbb1dccfd5c6c3409b382ce56a4336f
YanisMillasseau <millassy@3il.fr>
2026-02-25 13:52:24 +0100

    Update historique resumé

--- public/js/admin.js
+++ public/js/admin.js
@@ -364,12 +364,21 @@ function createDynamicMenu() {
                     </svg>
                     Mon compte
                 </a>
+                 <a href="/client">
+                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                        <line x1="16" y1="2" x2="16" y2="6"></line>
+                        <line x1="8" y1="2" x2="8" y2="6"></line>
+                        <line x1="3" y1="10" x2="21" y2="10"></line>
+                    </svg>
+                    Mes reservations
+                </a>
                 <form action="/api/logout" method="GET">
                     <button type="submit" class="logout-button">
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" class="svelte-rfuq4y"></path>
-                            <polyline points="16,17 21,12 16,7" class="svelte-rfuq4y"></polyline>
-                            <line x1="21" y1="12" x2="9" y2="12" class="svelte-rfuq4y"></line>
+                            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                            <polyline points="16,17 21,12 16,7"></polyline>
+                            <line x1="21" y1="12" x2="9" y2="12"></line>
                         </svg>
                         Déconnexion
                     </button>
