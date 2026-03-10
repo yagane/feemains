c1cd38648328232db721aa0b9af4635380ddc769
YanisMillasseau <millassy@3il.fr>
2026-03-10 14:27:00 +0100

    Update client visualisation

--- public/js/admin.js
+++ public/js/admin.js
@@ -290,6 +290,8 @@ async function loadTimeSlots(date) {
             const userInfo = document.querySelector('.user-info');
 
             userInfo.addEventListener('click', async function(event) {
+                console.log("userinfo");
+
                 const mainFooter = document.querySelector('.main-footer');
 
                 const modal = document.createElement('div');
